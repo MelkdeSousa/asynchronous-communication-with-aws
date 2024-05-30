6dd618767d5fb09bb513c240b01c363c003cb872
Melk de Sousa <melk.sousap@gmail.com>
2024-05-30 13:31:56 -0300

    add class for handle env vars

---
+++ points/src/Common/DotEnv.cs
@@ -0,0 +1,27 @@
+﻿namespace Common;
+
+public static class DotEnv
+{
+    /// <summary>
+    /// Loads environment variables from a file and sets them in the current environment.
+    /// </summary>
+    /// <param name="filePath">The path to the file containing the environment variables.</param>
+    public static void Load(string filePath)
+    {
+        if (!File.Exists(filePath))
+            return;
+
+        foreach (var line in File.ReadAllLines(filePath))
+        {
+            var parts = line.Split(
+                '=',
+                StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length != 2)
+                continue;
+
+            Environment.SetEnvironmentVariable(parts[0], parts[1]);
+        }
+    }
+
+}
