03c6423d81cf72cb7e72fe8f066818fa846c51e7
Melk de Sousa <melk.sousap@gmail.com>
2024-06-11 09:20:03 -0300

    add entrypoint

---
+++ users/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/melkdesousa/wottva/users/api/handlers"
+	"github.com/melkdesousa/wottva/users/internal/database"
+)
+
+func main() {
+
+	conn := database.DbConnection()
+
+	defer conn.Close()
+
+	// create a new user repository
+	userRepo := database.NewPgUserRepo(conn)
+
+	// create a new user controller
+	userController := handlers.NewUserController(userRepo)
+
+	app := http.NewServeMux()
+	app.Handle("/users", userController)
+	app.Handle("/users/", userController)
+	http.ListenAndServe(":8080", app) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
